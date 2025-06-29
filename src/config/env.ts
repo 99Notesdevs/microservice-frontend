aba2b97e22d7247848bffad9c02bb741b051f916
Vansh Maurya <72353067+va24nsh@users.noreply.github.com>
2025-06-30 00:09:49 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,8 @@
 export const env = {
-    API: "http://localhost:5500/api/v1",
-    API_MAIN: "http://localhost:5000/api/v1",
-    MAIN_PORTAL_API: "http://localhost:3000",
-    SOCKET_URL: "http://localhost:5500",
-    REACT_APP_GOOGLE_CLIENT_ID: "782725593034-g6v9lkkkld99ljbtma3h4bnmi8it8mmr.apps.googleusercontent.com"
+    API: process.env.REACT_PUBLIC_API_URL || "http://localhost:5500/api/v1",
+    API_MAIN: process.env.REACT_PUBLIC_API_MAIN_URL || "http://localhost:5000/api/v1",
+    MAIN_PORTAL_API: process.env.REACT_PUBLIC_MAIN_PORTAL_URL || "http://localhost:3000",
+    SOCKET_URL: process.env.REACT_PUBLIC_SOCKET_URL || "http://localhost:5500",
+    REACT_APP_GOOGLE_CLIENT_ID: process.env.REACT_PUBLIC_GOOGLE_CLIENT_ID || "782725593034-g6v9lkkkld99ljbtma3h4bnmi8it8mmr.apps.googleusercontent.com"
 };
   
